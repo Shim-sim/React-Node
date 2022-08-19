33a628f5579be5ece7772397a073e7d0f32e439c
Shimsim <shimdb96@gmail.com>
2022-08-19 14:03:17 +0000

    login router study

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const { User } = require('./models/User')
 const config = require('./config/key')
 
@@ -10,6 +11,7 @@ const config = require('./config/key')
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json json타입으로 된것을 분석해서 가져옴
 app.use(bodyParser.json());
+app.use(cookieParser())
 
 mongoose.connect(config.mongoURI, { useNewUrlParser: true })
 	.then(() => console.log('몽고커넥트됨'))
@@ -30,6 +32,37 @@ app.post('/register', (req, res) => {
 		return res.status(200).json({ success: true })
 	
 	}) 	
+})
+
+app.post('/login', (req, res) => {
+	
+	//요청 된 이메일을 데이터베이스에서 있는지 찾는다.
+	User.findOne( {email: req.body.email}, (err, user)=> {
+		if(!user) 
+			return res.json({
+				loginSuccess: false,
+				message: "제공된 이메일에 해당하는 유저가 없습니다."
+			})
+		
+		//요청 된 이메일이 데이터 베이스에 있다면 비밀번호도 확인
+		
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if(!isMatch) 
+			return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+			
+	
+			//비밀번호도 맞다면 토큰을 생성하기.
+			
+			user.generateToken((err, user) => {
+				if(err) return res.status(400).send(err)
+
+				// 토큰을 저장한다. 어디에? 쿠키, localstorage
+				res.cookie("x_auth", user.token)
+				.status(200)
+				.json({loginSuccess: true, userId: user._id})
+			})
+		})
+	})
 })
 
 
--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10  //salt가 몇 글자인지 설정
+const jwt = require('jsonwebtoken');
 
 
 const userSchema = mongoose.Schema({
@@ -38,6 +39,7 @@ const userSchema = mongoose.Schema({
  
 // 스키마의 method pre는 특정 동작 이전에 수행하는 것이다.
 // 지금 코드는 'save' 이전에 밑의 function을 수행한다는 의미이다.
+// 이 pre의 위치는 router의 save 전에 위치하게됨 next()로 이동
 userSchema.pre('save', function ( next ) {
 	var user = this;
 	
@@ -52,9 +54,35 @@ userSchema.pre('save', function ( next ) {
 			next()
     })
 	})	
+ } else {
+	 next()
  }
 	
 })
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+	
+	//plainPassword 1234567 암호화된 비밀번호가 같은지 체크하기 위해서 plain을 암호화한다.
+	bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+		console.log(this.password)
+		if(err) return callback(err)
+			callback(null, isMatch)
+	})
+	
+}
+
+userSchema.methods.generateToken = function(callback) {
+	var user = this
+	
+	// jwt 활용해서 토큰 생성하기 userid와 문자열로 토큰을 생성한다.
+	var token = jwt.sign(user._id.toHexString(), 'secretToken')
+	user.token = token
+	user.save(function(err, user) {
+		if(err) return callback(err)
+		callback(null, user)
+	})
+	
+}
 
 
 const User = mongoose.model('User', userSchema)
