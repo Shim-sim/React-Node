0591cae492bbf1d573aae1654c445ff5980c07df
Shim-sim <shimdb96@gmail.com>
2022-08-22 12:57:20 +0000

    update login, register Page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,19 +1,36 @@
 import React, {useEffect} from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage () {
+	const navigate = useNavigate()
 	
-	useEffect(()=> {
-		axios.get('https://nodeshim.run.goorm.io/api/hello')
+	const onClickHandler = () => {
+		axios.get('https://nodeshim.run.goorm.io/api/users/logout')
 		.then(response => {
-			console.log(response)
+			console.log(response.data)
+			if(response.data.success) {
+				navigate('/login')
+			} else {
+				alert('로그아웃 하는데 실패했습니다.')
+			}
 		})
-	}, [])
+	}
+	
+	
 	return (
-	<div>
-	첫 메인 페이지
+		<div style={{
+				display: 'flex', justifyContent: 'center', alignItems: 'center',
+					width: '100%', height: '100vh'
+			}}>
+		<h2>랜딩페이지</h2>
+
 			
-	</div>
+			
+			<button onClick={onClickHandler}>
+				로그아웃
+			</button>
+		</div>
 	)
 }
 
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { loginUser } from '../../../_actions/user_action'
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage () {
+	const navigate = useNavigate()
+	const dispatch = useDispatch()
+	const [Email, setEmail] = useState('')
+	const [Password, setPassword] = useState('')
+	
+
+	const onEmailHandler = (e) => {
+		setEmail(e.target.value)
+	}	
+	
+	const onPasswordHandler = (e) => {
+		setPassword(e.target.value)
+	}
+	
+	const onSubmitHandler = (e) => {
+		e.preventDefault()
+		
+		let body = {
+			email: Email,
+			password: Password
+		}
+		
+		dispatch(loginUser(body)) // dispatch의 return 값은  reducer의 loginUser에서 리턴된 객체가 됨
+		.then(response => {
+			if(response.payload.loginSuccess) {
+				navigate('/')
+			} else {
+				alert('로그인에 실패했습니다.')
+			}
+		})
+		
+	}
+	
 	return (
-	<div>
-	LoginPage
-			
-	</div>
+		<div style={{
+			display: 'flex', justifyContent: 'center', alignItems: 'center'
+			, width: '100%', height: '100vh'
+		}}>
+			<form style={{ display: 'flex', flexDirection: 'column' }}
+				onSubmit={onSubmitHandler}>
+				
+				<label>Email</label>
+				<input type="email" value={Email} onChange={onEmailHandler} />
+				
+				<label>Password</label>
+				<input type="password" value={Password} onChange={onPasswordHandler} />
+				<br />
+				
+				<button type="submit">
+						Login
+				</button>
+			</form>
+		</div>
 	)
 }
 
